93888a5094d473dad3b204b8eac8f52e86331f67
許景欣 <xjx0106@gmail.com>
2022-02-18 10:11:43 +0800

    perf: add package-lock to gitignore & import sass

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
 <template>
   <div id="app">
-    <div class="img-container">
-      <img alt="Vue logo" src="./assets/img/doge.jpg" />
-      <img alt="Vue logo" src="./assets/img/meme.jpg" />
-    </div>
-    <HelloWorld msg="天气预报哦" />
-    <div class="btns-container">
-      <div @click="getWeather" class="get-weather-btn">获取天气预报</div>
-      <div @click="clearWeather" class="get-weather-btn">清除</div>
-    </div>
-    <div class="request-time">{{ requestTime }}</div>
-    <div class="json-data-container">
-      <JsonViewer
-        :value="weatherData"
-        :expand-depth="5"
-        copyable
-        boxed
-        sort
-        class="json-data"
-      />
+    <div class="weather-container">
+      <div class="img-container">
+        <img class="img-meme" src="./assets/img/doge.jpg" />
+        <img class="img-meme" src="./assets/img/meme.jpg" />
+      </div>
+      <HelloWorld msg="天气预报哦" />
+      <div class="btns-container">
+        <div @click="getWeather" class="get-weather-btn">获取天气预报</div>
+        <div @click="clearWeather" class="get-weather-btn">清除</div>
+      </div>
+      <div class="request-time">{{ requestTime }}</div>
+      <div class="json-data-container">
+        <JsonViewer
+          :value="weatherData"
+          :expand-depth="5"
+          copyable
+          boxed
+          sort
+          class="json-data"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -47,7 +49,7 @@ export default {
     async getWeather() {
       const res = await jsonp(this.weatherUrl, {
         source: "pc",
-        weather_type: "observe|forecast_24h|air",
+        weather_type: "observe|forecast_24h|forecast_1h",
         province: "福建省",
         city: "漳州市",
         county: "龙海区",
@@ -56,24 +58,30 @@ export default {
         this.weatherData = res;
 
         const date = new Date();
-        const currentDate = date.getFullYear() + "年" + (date.getMonth() + 1) + "月" + date.getDate() + "日";
+        const currentDate =
+          date.getFullYear() +
+          "年" +
+          (date.getMonth() + 1) +
+          "月" +
+          date.getDate() +
+          "日";
 
         let hour = "";
-        if(date.getHours() < 10) {
+        if (date.getHours() < 10) {
           hour = "0" + date.getHours() + "";
         } else {
           hour = date.getHours();
         }
 
         let minute = "";
-        if(date.getMinutes() < 10) {
+        if (date.getMinutes() < 10) {
           minute = "0" + date.getMinutes();
         } else {
           minute = date.getMinutes();
         }
 
         let second = "";
-        if(date.getSeconds() < 10) {
+        if (date.getSeconds() < 10) {
           second = "0" + date.getSeconds();
         } else {
           second = date.getSeconds();
@@ -81,70 +89,70 @@ export default {
 
         const currentTime = hour + ":" + minute + ":" + second;
 
-        this.requestTime = "天气数据获取于： " + currentDate + "  " + currentTime;
+        this.requestTime =
+          "天气数据获取于： " + currentDate + "  " + currentTime;
       }
     },
     clearWeather() {
       this.requestTime = "";
       this.weatherData = {};
-    }
+    },
   },
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   margin-top: 60px;
-}
-.img-container {
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-}
-img {
-  width: 150px;
-  height: 150px;
-  margin: 15px;
-}
-iframe {
-  width: 200px;
-  height: 200px;
   border: 1px solid red;
 }
-.btns-container {
-  display: flex;
-  justify-content: center;
-}
-.get-weather-btn {
-  border: 1px solid #188cff;
-  height: 40px;
-  width: 120px;
-  border-radius: 4px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  user-select: none;
-  margin: 20px 20px 10px 20px;
-}
-.request-time {
+.weather-container {
   width: 100%;
-  text-align: center;
-  color: dimgray;
-  font-size: 15px;
-  height: 20px;
-}
-.json-data-container {
-  width: 100%;
-  display: flex;
-  justify-content: center;
-}
-.json-data {
-  width: 95%;
+  .img-container {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    > .img-meme {
+      width: 150px;
+      height: 150px;
+      margin: 15px;
+    }
+  }
+  .btns-container {
+    display: flex;
+    justify-content: center;
+    .get-weather-btn {
+      border: 1px solid #188cff;
+      height: 40px;
+      width: 120px;
+      border-radius: 4px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      user-select: none;
+      margin: 20px 20px 10px 20px;
+    }
+  }
+  .request-time {
+    width: 100%;
+    text-align: center;
+    color: dimgray;
+    font-size: 15px;
+    height: 20px;
+  }
+  .json-data-container {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    .json-data {
+      width: 95%;
+    }
+  }
 }
 </style>
